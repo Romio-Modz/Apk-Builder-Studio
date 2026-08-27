61cc426fdeec5fb2cb7f900df5c5fdfcfa138170
Romio-Modz <romio@modz.com>
2026-08-27 22:15:57 +0000

    v2.0.1: Fix crash - remove DynamicColors from App.kt, remove all watermark TextViews

--- app/src/main/java/com/apkbuilder/studio/App.kt
+++ app/src/main/java/com/apkbuilder/studio/App.kt
@@ -1,11 +1,9 @@
 package com.apkbuilder.studio
 
 import android.app.Application
-import com.google.android.material.color.DynamicColors
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
